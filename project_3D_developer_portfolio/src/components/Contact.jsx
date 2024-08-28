a8767c702e894597b3cdd591cfb39c1d684cc653
Rishabh Sahu <rishishahu112@gmail.com>
2024-08-29 00:52:17 +0530

    Update Contact.jsx

--- project_3D_developer_portfolio/src/components/Contact.jsx
+++ project_3D_developer_portfolio/src/components/Contact.jsx
@@ -72,9 +72,10 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
+        
         <h3 className={styles.sectionHeadText}>Contact.</h3>
-
+         <h4 className={styles.sectionHeadText}>risshishahuh112@gmail.com</h4>
+        <p className={styles.sectionSubText}>Get in touch</p>
         <form
           ref={formRef}
           onSubmit={handleSubmit}
